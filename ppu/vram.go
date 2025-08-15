85da769c966b0e7e56d94ccf8a0f466909823e98
dfireBird <me@dfirebird.dev>
2025-08-15 16:14:11 +0530

    fix: color 0 in all palette should be same

--- ppu/vram.go
+++ ppu/vram.go
@@ -14,8 +14,6 @@ const (
 
 	paletteRAMLoAddr = 0x3F00
 	paletteRAMHiAddr = 0x3FFF
-
-	paletteRAMIndexMask = 0x3F1F
 )
 
 type VRAM struct {
@@ -57,7 +55,10 @@ type PaletteRAM struct {
 
 func (p *PaletteRAM) ReadPRGMemory(addr types.Word) (bool, byte) {
 	if paletteRAMLoAddr <= addr && addr <= paletteRAMHiAddr {
-		idx := (addr & paletteRAMIndexMask) - paletteRAMLoAddr
+		idx := (addr - paletteRAMLoAddr) & 31
+		if idx%4 == 0 {
+			return true, p.data[0]
+		}
 		return true, p.data[idx]
 	}
 	return false, 0
@@ -65,13 +66,11 @@ func (p *PaletteRAM) ReadPRGMemory(addr types.Word) (bool, byte) {
 
 func (p *PaletteRAM) WritePRGMemory(addr types.Word, value byte) {
 	if paletteRAMLoAddr <= addr && addr <= paletteRAMHiAddr {
-		idx := (addr & paletteRAMIndexMask) - paletteRAMLoAddr
-		if idx == 0x00 {
-			p.data[0x10] = value
+		idx := (addr - paletteRAMLoAddr) & 31
+		if idx%4 == 0 {
+			p.data[0] = value
+		} else {
+			p.data[idx] = value
 		}
-		if idx == 0x10 {
-			p.data[0x00] = value
-		}
-		p.data[idx] = value
 	}
 }
